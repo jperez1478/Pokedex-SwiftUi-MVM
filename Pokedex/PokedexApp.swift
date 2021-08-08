40643158fcd83e09927ea45aab88d1b71927240c
jperez1478 <61983873+jperez1478@users.noreply.github.com>
2021-08-08 00:45:56 -0500

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Jessica Perez on 8/8/21.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
